a779e8faee1f23a2acfa95c53b5ff23a80ffef8e
KartikChugh <kartikchugh.kc@gmail.com>
2020-06-01 12:20:47 -0400

    Add conversation state enum

--- src/conversation.js
+++ src/conversation.js
@@ -1,11 +1,28 @@
+const StateEnum = Object.freeze({
+    GET_TASK : 1,
+    GET_TOPIC : 2,
+})
+
 class Conversation {
 
-    constructor() {
+    state = StateEnum.GET_TASK;
 
-    }
+    // constructor() {
+    //     state = StateEnum.GET_TASK;
+    // }
 
     respondTo(msg) {
-        return msg;
+
+        let response = "";
+
+        switch (this.state) {
+            case StateEnum.GET_TASK:
+                response = msg;
+                break;
+            
+        }
+
+        return response;
     }
 
 }
