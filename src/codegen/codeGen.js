7966e1cf1a85ffd826697cc869b77c23b6fb3d69
KartikChugh <kartikchugh.kc@gmail.com>
2020-06-13 14:02:44 -0400

    Generates model import code

--- src/codegen/codeGen.js
+++ src/codegen/codeGen.js
@@ -2,7 +2,7 @@ import * as importsCode from "codegen/importsCode";
 import * as regressionCode from "codegen/regressionCode"
 import * as knnCode from "codegen/knnCode";
 import * as sharedCode from "codegen/sharedCode";
-import {Models} from "state/StateTypes";
+import {Models, DatasetCategory} from "state/StateTypes";
 const StringBuilder = require("string-builder");
 
 const codeGen = (state) => {
@@ -10,4 +10,20 @@ const codeGen = (state) => {
     const sb = new StringBuilder();
     sb.appendLine(importsCode.standardImportsCode());
 
+    switch(state.model) {
+        case Models.KNN:
+            sb.appendLine(importsCode.knnImportsCode());
+        case Models.NEURAL_NETWORK_FF:
+            sb.appendLine(importsCode.nnImportsCode());
+        case Models.LINEAR_REGRESSION:
+            sb.appendLine(importsCode.linearImportsCode());
+        case Models.ORDINAL_REGRESSION:
+            sb.appendLine(importsCode.ordinalImportsCode());
+        case Models.POISSON_REGRESSION:
+            sb.appendLine(importsCode.poissonImportsCode());
+    } 
+
+
+
 }
+
