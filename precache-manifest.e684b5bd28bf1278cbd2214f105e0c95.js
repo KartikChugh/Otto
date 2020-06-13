16f48dba30b6ca5d0f523fa40b2175cbbe2c47e7
KartikChugh <kartikchugh.kc@gmail.com>
2020-06-12 23:02:09 -0400

    Updates

---
+++ precache-manifest.e684b5bd28bf1278cbd2214f105e0c95.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0193bfdd646c185ad6cf6a503f93a51c",
+    "url": "/otto/index.html"
+  },
+  {
+    "revision": "a1b6e1da2ce27ed0c133",
+    "url": "/otto/static/css/2.ba27f8b8.chunk.css"
+  },
+  {
+    "revision": "7d3ae8e663a29d1054db",
+    "url": "/otto/static/css/main.e8eb2a6d.chunk.css"
+  },
+  {
+    "revision": "a1b6e1da2ce27ed0c133",
+    "url": "/otto/static/js/2.d8a02f8c.chunk.js"
+  },
+  {
+    "revision": "9407abeca183f0b22bc53ee9985d6ce5",
+    "url": "/otto/static/js/2.d8a02f8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d3ae8e663a29d1054db",
+    "url": "/otto/static/js/main.5e2aa832.chunk.js"
+  },
+  {
+    "revision": "a9a5ee23d1ffb1f03959",
+    "url": "/otto/static/js/runtime-main.e540dd69.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/otto/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f8b1df51ba843179fa1cc9b53d58127a",
+    "url": "/otto/static/media/roboto-latin-100-italic.f8b1df51.woff2"
+  },
+  {
+    "revision": "f9e8e590b4e0f1ff83469bb2a55b8488",
+    "url": "/otto/static/media/roboto-latin-100-italic.f9e8e590.woff"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "/otto/static/media/roboto-latin-100-normal.5cb7edfc.woff"
+  },
+  {
+    "revision": "7370c3679472e9560965ff48a4399d0b",
+    "url": "/otto/static/media/roboto-latin-100-normal.7370c367.woff2"
+  },
+  {
+    "revision": "14286f3ba79c6627433572dfa925202e",
+    "url": "/otto/static/media/roboto-latin-300-italic.14286f3b.woff2"
+  },
+  {
+    "revision": "4df32891a5f2f98a363314f595482e08",
+    "url": "/otto/static/media/roboto-latin-300-italic.4df32891.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/otto/static/media/roboto-latin-300-normal.b00849e0.woff"
+  },
+  {
+    "revision": "ef7c6637c68f269a882e73bcb57a7f6a",
+    "url": "/otto/static/media/roboto-latin-300-normal.ef7c6637.woff2"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/otto/static/media/roboto-latin-400-italic.51521a2a.woff2"
+  },
+  {
+    "revision": "fe65b8335ee19dd944289f9ed3178c78",
+    "url": "/otto/static/media/roboto-latin-400-italic.fe65b833.woff"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/otto/static/media/roboto-latin-400-normal.479970ff.woff2"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/otto/static/media/roboto-latin-400-normal.60fa3c06.woff"
+  },
+  {
+    "revision": "288ad9c6e8b43cf02443a1f499bdf67e",
+    "url": "/otto/static/media/roboto-latin-500-italic.288ad9c6.woff"
+  },
+  {
+    "revision": "db4a2a231f52e497c0191e8966b0ee58",
+    "url": "/otto/static/media/roboto-latin-500-italic.db4a2a23.woff2"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/otto/static/media/roboto-latin-500-normal.020c97dc.woff2"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/otto/static/media/roboto-latin-500-normal.87284894.woff"
+  },
+  {
+    "revision": "81f57861ed4ac74741f5671e1dff2fd9",
+    "url": "/otto/static/media/roboto-latin-700-italic.81f57861.woff"
+  },
+  {
+    "revision": "da0e717829e033a69dec97f1e155ae42",
+    "url": "/otto/static/media/roboto-latin-700-italic.da0e7178.woff2"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/otto/static/media/roboto-latin-700-normal.2735a3a6.woff2"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/otto/static/media/roboto-latin-700-normal.adcde98f.woff"
+  },
+  {
+    "revision": "28f9151055c950874d2c6803a39b425b",
+    "url": "/otto/static/media/roboto-latin-900-italic.28f91510.woff"
+  },
+  {
+    "revision": "ebf6d1640ccddb99fb49f73c052c55a8",
+    "url": "/otto/static/media/roboto-latin-900-italic.ebf6d164.woff2"
+  },
+  {
+    "revision": "9b3766ef4a402ad3fdeef7501a456512",
+    "url": "/otto/static/media/roboto-latin-900-normal.9b3766ef.woff2"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "/otto/static/media/roboto-latin-900-normal.bb1e4dc6.woff"
+  }
+]);
