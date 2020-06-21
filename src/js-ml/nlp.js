a2a54591cfc74e08d6e5688a882e1a95a3a65d58
Sanuj Bhatia <sanujbhatia@umass.edu>
2020-06-20 19:56:13 -0700

    fix import

--- src/js-ml/nlp.js
+++ src/js-ml/nlp.js
@@ -1,11 +1,12 @@
+import { tweets } from "static/datasets/tweets.js";
+
 const { Wit, log } = require("node-wit");
 const token = require("TOKEN.json");
-import { tweets } from "static/datasets/tweets.js"
 
 const ottoNLP = new Wit({
-    accessToken: token.nlp,
+  accessToken: token.nlp,
 });
 
 export const invokeNLP = (doEntity, doSentiment) => {
-    alert(tweets.length);
-} 
+  alert(tweets.length);
+};
