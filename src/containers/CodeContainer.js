c3c8bf4dcb7143eb5b474314944cae7a5288eaa8
KartikChugh <kartikchugh.kc@gmail.com>
2020-06-23 11:29:44 -0400

    Styles code container

--- src/containers/CodeContainer.js
+++ src/containers/CodeContainer.js
@@ -4,6 +4,7 @@ import { Card, CardContent, Grow } from "@material-ui/core";
 import { useState } from "state/State";
 import { CodeGen } from "codegen/codeGen";
 import { useNNState } from "state/NNState";
+import { CopyBlock, CodeBlock, dracula, monokai, monokaiSublime, atomOneLight } from 'react-code-blocks';
 import { getActiveStep } from "./SummaryContainer";
 import { StepperStateOrder } from "state/StateTypes";
 
@@ -11,20 +12,32 @@ function CodeContainer({ getIsShown }) {
   const { state } = useState();
   const { nn_state } = useNNState();
 
+  const old =  
+//   <textarea
+//   value={CodeGen(state, nn_state)}
+//   style={{
+//     width: "100%",
+//     height: "100%",
+//     fontSize: "11px",
+//     backgroundColor: "#44318111",
+//     color: "black",
+//     fontFamily: "monospace",
+//   }}
+// />
+
+const updated = 
+<CopyBlock
+    text={CodeGen(state, nn_state)}
+		language={"python"}
+    showLineNumbers={false}
+    theme={atomOneLight}
+		wrapLines
+  />
+
   return (
     <Grow in={getIsShown()}>
-      <textarea
-        value={CodeGen(state, nn_state)}
-        style={{
-          width: "100%",
-          height: "100%",
-          fontSize: "11px",
-          backgroundColor: "#44318111",
-          color: "black",
-          fontFamily: "monospace",
-        }}
-      />
-    </Grow>
+    {updated}
+  </Grow>
   );
 }
 
