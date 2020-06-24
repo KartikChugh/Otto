ef9d26e83fda667661fb3eae8416ba44e8066b96
Sanuj Bhatia <31866817+sanujb@users.noreply.github.com>
2020-06-24 05:46:29 -0700

    added code styling

--- src/containers/CodeContainer.js
+++ src/containers/CodeContainer.js
@@ -1,51 +1,23 @@
 import React from "react";
-import { Card, CardContent, Grow } from "@material-ui/core";
+import { Grow } from "@material-ui/core";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { monokaiSublime } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 import { useState } from "state/State";
 import { CodeGen } from "codegen/codeGen";
 import { useNNState } from "state/NNState";
-import {
-  CopyBlock,
-  CodeBlock,
-  dracula,
-  monokai,
-  monokaiSublime,
-  atomOneLight,
-} from "react-code-blocks";
-import { getActiveStep } from "./SummaryContainer";
-import { StepperStateOrder } from "state/StateTypes";
 
 function CodeContainer({ getIsShown }) {
   const { state } = useState();
   const { nn_state } = useNNState();
 
-  //   const old =
-  //   <textarea
-  //   value={CodeGen(state, nn_state)}
-  //   style={{
-  //     width: "100%",
-  //     height: "100%",
-  //     fontSize: "11px",
-  //     backgroundColor: "#44318111",
-  //     color: "black",
-  //     fontFamily: "monospace",
-  //   }}
-  // />
-
-  const updated = (
-    <CopyBlock
-      text={CodeGen(state, nn_state)}
-      language={"python"}
-      showLineNumbers={false}
-      theme={monokaiSublime}
-      highlight={""}
-      wrapLines
-    />
-  );
-
   return (
     <Grow in={getIsShown()}>
-      <div className="codeOverflow">{updated}</div>
+      <div className="codeOverflow">
+        <SyntaxHighlighter language="python" style={monokaiSublime}>
+          {CodeGen(state, nn_state)}
+        </SyntaxHighlighter>
+      </div>
     </Grow>
   );
 }
